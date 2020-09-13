801bdbe3f43b8a51dae2047154e0c5ea50d981a2
Daniel Gonzalez Adarve <dgonzalezad@gmail.com>
2020-09-13 19:59:46 +1000

    changed: section component tag to section modified: tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-let alpha = {100: 'FF', 90: 'E6', 80: 'CC', 70: 'B3', 60: '99', 50: '80', 40: '66', 30: '4D', 20: '33', 10: '1A'};
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '');
+const em = (px, base) => `${round(px / base)}em`;
+const alpha = {
+  100: 'FF',
+  90: 'E6',
+  80: 'CC',
+  70: 'B3',
+  60: '99',
+  50: '80',
+  40: '66',
+  30: '4D',
+  20: '33',
+  10: '1A',
+};
 
 module.exports = {
   purge: {
@@ -42,97 +59,6 @@ module.exports = {
     screens: {
       ...defaultTheme.screens,
     },
-    spacing: {
-      px: '1px',
-      '0': '0',
-      '1': '0.25rem',
-      '2': '0.5rem',
-      '3': '0.75rem',
-      '4': '1rem',
-      '5': '1.25rem',
-      '6': '1.5rem',
-      '7': '1.75rem',
-      '8': '2rem',
-      '9': '2.25rem',
-      '10': '2.5rem',
-      '11': '2.75rem',
-      '12': '3rem',
-      '13': '3.25rem',
-      '14': '3.5rem',
-      '15': '3.75rem',
-      '16': '4rem',
-      '17': '4.25rem',
-      '18': '4.5rem',
-      '19': '4.75rem',
-      '20': '5rem',
-      '21': '5.25rem',
-      '22': '5.50rem',
-      '23': '5.75rem',
-      '24': '6rem',
-      '25': '6.25rem',
-      '26': '6.50rem',
-      '27': '6.75rem',
-      '28': '7rem',
-      '29': '7.25rem',
-      '30': '7.50rem',
-      '31': '7.75rem',
-      '32': '8rem',
-      '33': '8.25rem',
-      '34': '8.50rem',
-      '35': '8.75rem',
-      '36': '9rem',
-      '37': '9.25rem',
-      '38': '9.50rem',
-      '39': '9.75rem',
-      '40': '10rem',
-      '41': '10.15rem',
-      '42': '10.25rem',
-      '43': '10.35rem',
-      '44': '10.45rem',
-      '45': '10.55rem',
-      '46': '10.65rem',
-      '47': '11rem',
-      '48': '12rem',
-      '49': '12.5rem',
-      '50': '13rem',
-      '51': '13.5rem',
-      '52': '14.5rem',
-      '53': '15rem',
-      '54': '15.5rem',
-      '55': '16rem',
-      '56': '17rem',
-      '57': '18rem',
-      '58': '19rem',
-      '59': '20rem',
-      '60': '21rem',
-      '61': '22rem',
-      '62': '23rem',
-      '63': '24rem',
-      '64': '25rem',
-    },
-    opacity: {
-      '0': '0',
-      '5': '.05',
-      '10': '.1',
-      '15': '.15',
-      '20': '.2',
-      '25': '.25',
-      '30': '.3',
-      '35': '.35',
-      '40': '.4',
-      '45': '.45',
-      '50': '.5',
-      '55': '.55',
-      '60': '.6',
-      '65': '.65',
-      '70': '.7',
-      '75': '.75',
-      '80': '.8',
-      '85': '.85',
-      '90': '.9',
-      '95': '.95',
-      '100': '1',
-    },
     extend: {
       // https://javisperez.github.io/tailwindcolorshades
       colors: {
@@ -168,18 +94,109 @@ module.exports = {
           pinterest: '#cc2127'
         }
       },
+      spacing: {
+        px: '1px',
+        '0': '0',
+        '1': '0.25rem',
+        '2': '0.5rem',
+        '3': '0.75rem',
+        '4': '1rem',
+        '5': '1.25rem',
+        '6': '1.5rem',
+        '7': '1.75rem',
+        '8': '2rem',
+        '9': '2.25rem',
+        '10': '2.5rem',
+        '11': '2.75rem',
+        '12': '3rem',
+        '13': '3.25rem',
+        '14': '3.5rem',
+        '15': '3.75rem',
+        '16': '4rem',
+        '17': '4.25rem',
+        '18': '4.5rem',
+        '19': '4.75rem',
+        '20': '5rem',
+        '21': '5.25rem',
+        '22': '5.50rem',
+        '23': '5.75rem',
+        '24': '6rem',
+        '25': '6.25rem',
+        '26': '6.50rem',
+        '27': '6.75rem',
+        '28': '7rem',
+        '29': '7.25rem',
+        '30': '7.50rem',
+        '31': '7.75rem',
+        '32': '8rem',
+        '33': '8.25rem',
+        '34': '8.50rem',
+        '35': '8.75rem',
+        '36': '9rem',
+        '37': '9.25rem',
+        '38': '9.50rem',
+        '39': '9.75rem',
+        '40': '10rem',
+        '41': '10.15rem',
+        '42': '10.25rem',
+        '43': '10.35rem',
+        '44': '10.45rem',
+        '45': '10.55rem',
+        '46': '10.65rem',
+        '47': '11rem',
+        '48': '12rem',
+        '49': '12.5rem',
+        '50': '13rem',
+        '51': '13.5rem',
+        '52': '14.5rem',
+        '53': '15rem',
+        '54': '15.5rem',
+        '55': '16rem',
+        '56': '17rem',
+        '57': '18rem',
+        '58': '19rem',
+        '59': '20rem',
+        '60': '21rem',
+        '61': '22rem',
+        '62': '23rem',
+        '63': '24rem',
+        '64': '25rem',
+      },
+      opacity: {
+        '0': '0',
+        '5': '.05',
+        '10': '.1',
+        '15': '.15',
+        '20': '.2',
+        '25': '.25',
+        '30': '.3',
+        '35': '.35',
+        '40': '.4',
+        '45': '.45',
+        '50': '.5',
+        '55': '.55',
+        '60': '.6',
+        '65': '.65',
+        '70': '.7',
+        '75': '.75',
+        '80': '.8',
+        '85': '.85',
+        '90': '.9',
+        '95': '.95',
+        '100': '1',
+      },
       fontSize: theme => ({
-        'xs': ['0.75rem', theme('lineHeight.normal')],    // 12
-        'sm': ['0.875rem', theme('lineHeight.normal')],   // 14
-        'base': ['1rem', theme('lineHeight.normal')],     // 16
-        'md': ['1.125rem', theme('lineHeight.normal')],   // 18
-        'lg': ['1.25rem', theme('lineHeight.normal')],    // 20
-        'xl': ['1.5rem', theme('lineHeight.normal')],     // 24
-        '2xl': ['1.875rem', theme('lineHeight.normal')],  // 30
-        '3xl': ['2.25rem', theme('lineHeight.normal')],   // 36
-        '4xl': ['3rem', theme('lineHeight.normal')],      // 48
-        '5xl': ['4rem', theme('lineHeight.normal')],      // 64
-        '6xl': ['4.5rem', theme('lineHeight.normal')]     // 72
+        'xs': ['0.75rem', theme('lineHeight.normal')], // 12
+        'sm': ['0.875rem', theme('lineHeight.normal')], // 14
+        'base': ['1rem', theme('lineHeight.normal')], // 16
+        'md': ['1.125rem', theme('lineHeight.normal')], // 20
+        'lg': ['1.375rem', theme('lineHeight.normal')], // 22
+        'xl': ['1.875rem', theme('lineHeight.normal')], // 30
+        '2xl': ['2rem', theme('lineHeight.normal')], // 32
+        '3xl': ['2.25rem', theme('lineHeight.normal')], // 36
+        '4xl': ['3rem', theme('lineHeight.normal')], // 48
+        '5xl': ['4rem', theme('lineHeight.normal')], // 64
+        '6xl': ['4.5rem', theme('lineHeight.normal')], // 72
       }),
       fontFamily: {
         'sans-primary': [
@@ -212,19 +229,53 @@ module.exports = {
         '0': 0,
         '1/2': '50%'
       },
-      typography: theme => ({
+      // Plugins
+      typography: (theme) => ({
         default: {
           css: {
+            maxWidth: '100%',
+            color: theme('colors.black'),
+            h1: {
+              fontSize: em(32, 16),
+              lineHeight: '1.5',
+            },
+            h2: {
+              fontSize: em(20, 16),
+              lineHeight: '1.5',
+            },
+            h3: {
+              fontSize: em(22, 16),
+              lineHeight: '1.5',
+            },
+            h4: {
+              fontSize: em(20, 16),
+              lineHeight: '1.5',
+            },
+            h5: {
+              fontSize: em(16, 16),
+              lineHeight: '1.5',
+              fontWeight: 'bold'
+            },
+            h6: {
+              fontSize: em(16, 16),
+              lineHeight: '1.5',
+              fontWeight: 'bold'
+            },
             a: {
-              color: theme('colors.primary.500'),
+              color: theme('colors.black'),
               '&:hover': {
-                color: theme('colors.primary.600'),
-              }
+                color: theme('colors.gray.800'),
+              },
+            },
+            'ul > li::before': {
+              backgroundColor: theme('colors.black'),
+            },
+            'ol > li::before': {
+              color: theme('colors.black'),
             }
-          }
-        }
+          },
+        },
       }),
-      // Plugins
       aspectRatio: {
         'square': [1, 1],
         '16/9': [16, 9],
